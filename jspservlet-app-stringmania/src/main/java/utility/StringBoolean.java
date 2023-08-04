a969eeaa0043d352e35b0430de5b4be80893180a
Iamneo <techadmin@iamneo.ai>
2023-08-04 04:50:07 +0000

    Backup Commit

--- jspservlet-app-stringmania/src/main/java/utility/StringBoolean.java
+++ jspservlet-app-stringmania/src/main/java/utility/StringBoolean.java
@@ -1,58 +1,17 @@
 package utility;
 
 public class StringBoolean {
-    private String input1;
-    private String input2;
-
-    // Getter and Setter for input1
-    public String getInput1() {
+     private String input1;
+     private String input2;
+     public String getInput1() {
         return input1;
     }
-
     public void setInput1(String input1) {
-        this.input1 = input1;
-    }
-
-    // Getter and Setter for input2
-    public String getInput2() {
-        return input2;
-    }
-
-    public void setInput2(String input2) {
-        this.input2 = input2;
-    }
-
-    // Method to check if the string1 contains string2
-    public boolean containsString() {
-        return input1.contains(input2);
-    }
-
-    // Method to check if the string starts with the given letter
-    public boolean startsWithLetter() {
-        if (input1.isEmpty() || input2.isEmpty()) {
-            return false;
-        }
-        char startChar = input2.charAt(0);
-        return input1.charAt(0) == startChar;
-    }
-
-    // Method to check if the string end with the given letter
-    public boolean endsWithLetter() {
-        if (input1.isEmpty() || input2.isEmpty()) {
-            return false;
-        }
-        char endChar = input2.charAt(0);
-        return input1.charAt(input1.length() - 1) == endChar;
-    }
-
-    // Method to check if two strings are equal
-    public boolean areEqual() {
-        return input1.equals(input2);
-    }
-
-    // Method to check if two strings are equal by ignoring the case
-    public boolean areEqualIgnoreCase() {
-        return input1.equalsIgnoreCase(input2);
-    }
-}
-
+        this.input1 = input1;    }
+        public String getInput2() {        return input2;    }
+        public void setInput2(String input2) {        this.input2 = input2;    }
+        public boolean contains(String input1 , String input2) {        return input1.contains(input2);    }
+        public boolean startsWith(String ip1, String ip2) {        return ip1.startsWith(String.valueOf(ip2));    }
+        public boolean endsWith(String i1,String i2) {        return i1.endsWith(String.valueOf(i2));    }
+        public boolean equals(String ip1,String ip2) {        return ip1.equals(ip2);    }
+        public boolean equalsIgnoreCase(String input1,String input2) {        return input1.equalsIgnoreCase(input2);    }}
